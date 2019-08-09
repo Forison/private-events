0fc86470eafc18f0b037942d7ff4ba1715d632ba
addo <forison6205@gmail.com>
2019-08-09 07:35:15 -0400

    added session logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
   root "home#index"
-  get 'sessions/new'
   resource :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :sessions
 end
