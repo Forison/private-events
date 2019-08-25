32be004f51899d6d65656f6d4f1dfabc363f0050
samuel <samuelgarciacompanys@gmail.com>
2019-08-25 21:42:07 +0200

    corrected problem with form to submit new events

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -4,19 +4,7 @@ class AttendancesController < ApplicationController
   def new
     @attendance = Attendance.new
   end
-  # def attend
-  #   @event = Event.find(params[:attendance][:event_id])
-  #   @event.attendances(user_id: params[:attendance][:user_id] )
-  #   current_user.attendances(event_id: params[:attendance][:event_id] )
-  #   @attendance.user_id = params[:attendance][:user_id]
-  #   @attendance.event_id = params[:attendance][:event_id] 
-  #   if @attendance.save
-  #     flash.now[:success] = 'You have joined the event!'
-  #     redirect_to users_path(current_user)
-  #   else
-  #     flash.now[:danger] = "There was a problem"
-  #   end 
-  # end
+
 
   def attend
     @attendance = Attendance.new
